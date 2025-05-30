608aea5f24e752cfd14999eabb0ff2fa8f1b1b9a
artem-shig <artem.shigov@gmail.com>
2025-05-30 19:45:09 +0300

    nitialization

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -2,11 +2,17 @@
 
 int main() {
 
-	Student st1, st2, st3;
+	Student st1;
+	Student st2("Alex", "Chichikov");
+	Student st3("Ivan", "Ivanov", 15);
+	Student st4("Artem", "Shigov", 15, 9, 'm', true, 9.6);
+	Student st5(st4);
 
 	cout << st1.toString() << endl;
 	cout << st2.toString() << endl;
 	cout << st3.toString() << endl;
+	cout << st4.toString() << endl;
+	cout << st5.toString() << endl;
 
 	return 0;
 }
--- MainProject/student.h
+++ MainProject/student.h
@@ -24,6 +24,54 @@ public:
 		mark = 0;
 	}
 
+	Student(string fname, string sname) {
+
+		name = fname;
+		surname = sname;
+		age = 0;
+		_class = 0;
+		gender = 'm';
+		alive = false;
+		mark = 0;
+	}
+
+	Student(string fname, string sname, int a) {
+
+		name = fname;
+		surname = sname;
+		age = a;
+		_class = 0;
+		gender = 'm';
+		alive = false;
+		mark = 0;
+	}
+
+	Student(string fname, string sname, int a, int cl, char gen,
+		bool al, double mk) {
+
+		name = fname;
+		surname = sname;
+		age = a;
+		_class = cl;
+		gender = gen;
+		alive = al;
+		mark = mk;
+	}
+
+	Student(const Student& student) {
+		name = student.name;
+		surname = student.surname;
+		age = student.age;
+		_class = student._class;
+		gender = student.gender;
+		alive = student.alive;
+		mark = student.mark;
+	}
+
+	~Student() {
+		cout << "destructor" << endl;
+	}
+
 	string toString() {
 
 		string s = name + " " + surname;
